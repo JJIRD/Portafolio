35d763a1659de1d58210abb2c2df6d6cb54f85c2
JJIRD <josejird.p@gmail.com>
2023-05-25 16:00:42 -0600

    part1

---
+++ Portfolio 2.0/js/script.js
@@ -0,0 +1,7 @@
+/* ======================== typing animate ======================= */
+var typed = new Typed(".typing", {
+    strings:["Web Designer","","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
