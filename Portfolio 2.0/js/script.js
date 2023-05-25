50f6d4b4e410c6c4b9331f793302800398513e36
JJIRD <josejird.p@gmail.com>
2023-05-25 17:36:05 -0600

    part2

--- Portfolio 2.0/js/script.js
+++ Portfolio 2.0/js/script.js
@@ -5,3 +5,15 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true
 })
+/* ======================== Aside ======================= */
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length;
+      for(let i=0; i<totalNavList; i++)
+      {
+        const a=navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            this.classList.add("active")
+        })
+      }
